3413ad49658e5aaaf3bd80874d5751f5b8748cc7
Dmitriy Pismenniy <dima30655@gmail.com>
2020-05-24 19:17:20 +0300

    Updates

---
+++ precache-manifest.8ab375f932ce3ad58ce97d0a813fd65b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34801d3dbfe548bda71702f0f62e2640",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "96d0dec6599bfc761567",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.db7b5b7b.chunk.css"
+  },
+  {
+    "revision": "06fdf44f3f176ed1491a",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.a941d402.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.a941d402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d0dec6599bfc761567",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f6e00ed6.chunk.js"
+  },
+  {
+    "revision": "25868c5f5ac532d6d81a",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9cda7063.js"
+  }
+]);
